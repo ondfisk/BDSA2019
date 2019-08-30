f79905070a533f1821ac172c2c1c908ad94d3ca6
Rasmus Lystrøm <rasmusl@microsoft.com>
2019-08-30 10:29:28 +0200

    solution skeleton for lecture 1

---
+++ BDSA2019.Lecture01/BDSA2019.Lecture01.App.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BDSA2019.Lecture01.App.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BDSA2019.Lecture01/BDSA2019.Lecture01.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSA2019.Lecture01.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

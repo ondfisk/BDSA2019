ade6499f8bfb5f204972e16c8a967f4e8eef21a2
Rasmus Lystrøm <rasmusl@microsoft.com>
2019-11-22 11:51:59 +0100

    Lecture 10 done

---
+++ BDSA2019.Lecture10/BDSA2019.Lecture10.DemoApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BDSA2019.Lecture10.DemoApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var current = Light.Fill as SolidColorBrush;
+
+            if (current.Color == Colors.Red)
+                Light.Fill = new SolidColorBrush(Colors.Green);
+            else
+                Light.Fill = new SolidColorBrush(Colors.Red);
+
+        }
+    }
+}
